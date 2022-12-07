a8fe20900c695786beb254c2ead4634e18effcfe
sudha123 <sudhanaukala1997@gmail.com>
2022-12-07 18:01:38 +0530

    Added:CustomException

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -4,14 +4,6 @@ public class MoodAnalyser {
 
     String message;
 
-    public static void main(String[] args) {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String happyMood = moodAnalyser.analyseMood();
-        System.out.println("My mood is :" + happyMood);
-        String sadMood = moodAnalyser.analyseMood();
-        System.out.println("My Mood is :" + sadMood);
-    }
-
     public MoodAnalyser(String message) {
         this.message = message;
     }
@@ -19,15 +11,15 @@ public class MoodAnalyser {
     public MoodAnalyser() {
     }
 
-    public String analyseMood() {
+    public String analyseMood() throws MoodAnalyserException {
         try {
             if (message.toLowerCase().contains("sad"))
                 return "SAD";
             else
                 return "HAPPY";
-        } catch (Exception e) {
-            return "HAPPY";
+        }
+        catch (Exception e){
+            throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.NULL,"Message is Null");
         }
     }
 }
-
